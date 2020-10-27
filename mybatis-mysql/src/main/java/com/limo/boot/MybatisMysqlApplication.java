7c3d2075e340d806e66ee16a0e3abfd5bf3be7fa
limos-github <lib97391459@gmail.com>
2020-10-27 14:12:54 +0800

    1.配置druid数据库连接池 2.配置logback日志打印

--- mybatis-mysql/src/main/java/com/limo/boot/MybatisMysqlApplication.java
+++ mybatis-mysql/src/main/java/com/limo/boot/MybatisMysqlApplication.java
@@ -3,6 +3,8 @@ package com.limo.boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 /**
  * @author lib97
  */
--- mybatis-mysql/src/main/java/com/limo/boot/controller/HelloController.java
+++ mybatis-mysql/src/main/java/com/limo/boot/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.limo.boot.controller;
 
 import com.limo.boot.mapper.AdminUserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Autowired
     public AdminUserMapper adminUserMapper;
 
@@ -17,8 +20,10 @@ public class HelloController {
     @RequestMapping("/hello/{id}")
     public String helloAdmin(@PathVariable int id){
         try {
+            logger.debug("查询取得数据!");
             return adminUserMapper.selectByPrimaryKey(id).toString();
         }catch (Exception e){
+            logger.error("异常{}",e);
             return "查询失败，请检查参数！";
         }
     }
